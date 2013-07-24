17515e08ac7beae00a34f271c4508efb97fe5415
vlad-pataliu <vlad-pataliu@gmail.com>
2013-07-24 12:32:04 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterCrawling::Application.config.secret_token = '407be8cc158d75ccf8a263a47bd1a16b6897e605be9d1c0485066ab61d610a976db19fb9c016533aeacbd112e7df2155c390987b1fd24d157df50a92302a1aaa'
