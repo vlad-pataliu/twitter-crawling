ae5044a7b3cb0573db703561b869e70330d9d8c3
vlad-pataliu <vlad-pataliu@gmail.com>
2013-07-30 12:42:10 +0100

    bla

---
+++ app/models/twitter_crawling_db.rb
@@ -0,0 +1,11 @@
+class TwitterCrawling
+  include Mongoid::Document
+  include Mongoid::ActiveRecordBridge
+
+  field :name,          type: String
+  field :username,      type: String
+  field :song_name,     type: String
+  field :artist_name,   type: String
+  field :user_location, type: String
+
+end
