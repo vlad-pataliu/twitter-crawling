7003f6cc84401aee6817dde3a2c6074796759893
David Ko <davidko26@gmail.com>
2013-08-16 21:46:41 -0400

    Update model

--- app/models/twitter_crawl.rb
+++ app/models/twitter_crawl.rb
@@ -1,11 +1,11 @@
 class TwitterCrawl
   include Mongoid::Document
   
-  field :name,          type: String
-  field :username,      type: String
-  field :user_location, type: String
-  field :artist,		type: Hash
-  field :track,			type: Hash
-
-  field :artist_tags,	type: Array
+  field :name,            type: String
+  field :username,        type: String
+  field :user_location,   type: String
+  field :date,            type: Time
+  field :song,		        type: Hash
+  field :location,        type: Hash
+  field :tags,	          type: Array
 end
